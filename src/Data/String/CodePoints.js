71cdcf299d1acb32e702b4055fe65a721fa171b6
Michael Ficarra <mficarra@shapesecurity.com>
2017-05-28 14:45:12 -0600

    bugfixes

--- src/Data/String/CodePoints.js
+++ src/Data/String/CodePoints.js
@@ -17,6 +17,15 @@ function fromCodePoint(cp) {
   return cu1 + cu2;
 }
 
+var codePointAt0 = hasCodePointAt
+  ? function (str) { return str.codePointAt(0); }
+  : function (str) {
+    if (str.length === 1) {
+      return str.charCodeAt(0);
+    }
+    return ((str.charCodeAt(0) - 0xD800) * 0x400 + (str.charCodeAt(1) - 0xDC00) + 0x10000);
+  };
+
 exports._codePointAt = function (fallback) {
   return function (Just) {
     return function (Nothing) {
@@ -24,16 +33,16 @@ exports._codePointAt = function (fallback) {
         return function (str) {
           var length = str.length;
           if (index < 0 || index >= length) return Nothing;
-          if (hasArrayFrom && hasCodePointAt) {
+          if (hasArrayFrom) {
             var cps = Array.from(str);
             if (index >= cps.length) return Nothing;
-            return Just(cps[index].codePointAt(0));
+            return Just(codePointAt0(cps[index]));
           } else if (hasStringIterator) {
             var iter = str[Symbol.iterator]();
             for (var i = index;; --i) {
               var o = iter.next();
               if (o.done) return Nothing;
-              if (i === 0) return Just(o.value);
+              if (i === 0) return Just(codePointAt0(o.value));
             }
           }
           return fallback(index)(str);
@@ -70,6 +79,7 @@ exports._count = function (isLead) {
 };
 
 exports.fromCodePointArray = hasFromCodePoint
+  // TODO: using F.p.apply here will fail for very large strings; use alternative implementation for very large strings
   ? function (cps) { return String.fromCodePoint.apply(String, cps); }
   : function (cps) { return cps.map(fromCodePoint).join(""); };
 
@@ -93,23 +103,23 @@ exports._take = function (fallback) {
         return accum;
       };
     }
-    return fallback;
+    return fallback(n);
   };
 };
 
 exports._toCodePointArray = function (fallback) {
-  if (hasArrayFrom && hasCodePointAt) {
+  if (hasArrayFrom) {
     return function (str) {
-      return Array.from(str, function (x) { return x.codePointAt(0); });
+      return Array.from(str, codePointAt0);
     };
-  } else if (hasStringIterator && hasCodePointAt) {
+  } else if (hasStringIterator) {
     return function (str) {
       var accum = [];
       var iter = str[Symbol.iterator]();
       for (;;) {
         var o = iter.next();
         if (o.done) return accum;
-        accum.push(o.value.codePointAt(0));
+        accum.push(codePointAt0(o.value));
       }
     };
   }

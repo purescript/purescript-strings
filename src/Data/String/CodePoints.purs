66b7bbc327e5d182e7b529b87f69a72b0c79f6ea
Carsten Csiky <carsten.csiky@googlemail.com>
2017-12-23 22:14:26 +0100

    Show Instance for CodePoint (#93)

--- src/Data/String/CodePoints.purs
+++ src/Data/String/CodePoints.purs
@@ -29,6 +29,7 @@ import Prelude
 
 import Data.Array as Array
 import Data.Char as Char
+import Data.Int (hexadecimal, toStringAs)
 import Data.Maybe (Maybe(Just, Nothing))
 import Data.String as String
 import Data.String.Unsafe as Unsafe
@@ -49,6 +50,9 @@ newtype CodePoint = CodePoint Int
 
 derive instance eqCodePoint :: Eq CodePoint
 derive instance ordCodePoint :: Ord CodePoint
+
+instance showCodePoint :: Show CodePoint where
+  show (CodePoint i) = "(CodePoint 0x" <> String.toUpper (toStringAs hexadecimal i) <> ")"
 
 -- I would prefer that this smart constructor not need to exist and instead
 -- CodePoint just implements Enum, but the Enum module already depends on this

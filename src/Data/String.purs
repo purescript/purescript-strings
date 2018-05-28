e84bd083163a52e0d4754a165eb154dc1d6b3dfe
Carsten <Carsten.Koenig@hotmail.de>
2018-05-28 07:41:27 +0200

    reexport Data.String.CodeUnits
    those reexports are missing - for example `charAt` is not found right now

--- src/Data/String.purs
+++ src/Data/String.purs
@@ -1,10 +1,12 @@
 module Data.String
   ( module Data.String.Common
   , module Data.String.CodePoints
+  , module Data.String.CodeUnits
   , module Data.String.Pattern
   ) where
 
 import Data.String.CodePoints
+import Data.String.CodeUnits
 
 import Data.String.Common (joinWith, localeCompare, null, replace, replaceAll, split, toLower, toUpper, trim)
 import Data.String.Pattern (Pattern(..), Replacement(..))

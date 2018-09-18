765d7159ef78afaa2e9f0c2818ae1a19b106c3d9
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2018-09-18 14:31:37 +0000

    Fix string split, implement replace

--- src/Data/String/Common.erl
+++ src/Data/String/Common.erl
@@ -2,14 +2,13 @@
 -export(['_localeCompare'/5, replace/3, replaceAll/3, split/2, toLower/1, toUpper/1, trim/1, joinWith/2]).
 
 '_localeCompare'(_Lt,_Eq,_Gt,_S1,_S2) -> error("not implemented").
-replace(_S1,_S2,_S3) -> error("not implemented").
-replaceAll(_S1,_S2,_S3)-> error("not implemented").
+replace(S1,S2,S3) -> string:replace(S3, S1, S2).
+replaceAll(S1,S2,S3) -> string:replace(S3, S1, S2, all).
 
 split(Sep,S) ->
-  Split = re:split(S, Sep),
+  Split = string:split(S, Sep, all),
   Res = case {Sep,S} of
     {_,<<>>} -> lists:droplast(Split);
-    {<<>>,_} -> lists:droplast(Split);
     _ -> Split
   end,
   array:from_list(Res).

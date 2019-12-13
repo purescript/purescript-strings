fc1bead0fb180e292edea4305ceb36cc103169d1
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2019-12-13 12:25:06 +0000

    purerl fix replace, split

--- src/Data/String/Common.erl
+++ src/Data/String/Common.erl
@@ -2,13 +2,18 @@
 -export(['_localeCompare'/5, replace/3, replaceAll/3, split/2, toLower/1, toUpper/1, trim/1, joinWith/2]).
 
 '_localeCompare'(_Lt,_Eq,_Gt,_S1,_S2) -> error("not implemented").
-replace(S1,S2,S3) -> string:replace(S3, S1, S2).
-replaceAll(S1,S2,S3) -> string:replace(S3, S1, S2, all).
+replace(S1,S2,S3) -> iolist_to_binary(string:replace(S3, S1, S2)).
+replaceAll(S1,S2,S3) -> iolist_to_binary(string:replace(S3, S1, S2, all)).
 
 split(Sep,S) ->
   Split = string:split(S, Sep, all),
   Res = case {Sep,S} of
-    {_,<<>>} -> lists:droplast(Split);
+    {<<>>,<<>>} -> []; %% string:split says [<<>>] but JS says []
+    {<<>>,_} -> 
+      %% string:split does not work on empty split pattern
+      lists:map(fun (C) -> unicode:characters_to_binary([C], utf8) end, unicode:characters_to_list(S, utf8));
+     
+    %% {_,<<>>} behaves the same in JS vs string:split
     _ -> Split
   end,
   array:from_list(Res).

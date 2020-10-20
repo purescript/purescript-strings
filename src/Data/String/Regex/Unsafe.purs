e49d5aa7a0a3edbf1a4b16b49daafac1ad7c8322
toastal <toastal@protonmail.com>
2020-10-20 23:43:30 +0700

    either unsafeCrashWith

--- src/Data/String/Regex/Unsafe.purs
+++ src/Data/String/Regex/Unsafe.purs
@@ -2,13 +2,13 @@ module Data.String.Regex.Unsafe
   ( unsafeRegex
   ) where
 
-import Data.Maybe (fromJust)
-import Data.Either (hush)
+import Prelude (identity)
+import Data.Either (either)
 import Data.String.Regex (Regex, regex)
 import Data.String.Regex.Flags (RegexFlags)
-import Partial.Unsafe (unsafePartial)
+import Partial.Unsafe (unsafeCrashWith)
 
 -- | Constructs a `Regex` from a pattern string and flags. Fails with
 -- | an exception if the pattern contains a syntax error.
 unsafeRegex :: String -> RegexFlags -> Regex
-unsafeRegex s f = unsafePartial fromJust (hush (regex s f))
+unsafeRegex s f = either (unsafeCrashWith identity) (regex s f)

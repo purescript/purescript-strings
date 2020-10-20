faeb8f6f2c4ce77d1e183af758cb6729db82628e
toastal <toastal@protonmail.com>
2020-10-20 14:08:03 +0700

    catch the lack of partial for future Either

--- src/Data/String/Regex/Unsafe.purs
+++ src/Data/String/Regex/Unsafe.purs
@@ -2,7 +2,8 @@ module Data.String.Regex.Unsafe
   ( unsafeRegex
   ) where
 
-import Data.Either (fromRight)
+import Data.Maybe (fromJust)
+import Data.Either (hush)
 import Data.String.Regex (Regex, regex)
 import Data.String.Regex.Flags (RegexFlags)
 import Partial.Unsafe (unsafePartial)
@@ -10,4 +11,4 @@ import Partial.Unsafe (unsafePartial)
 -- | Constructs a `Regex` from a pattern string and flags. Fails with
 -- | an exception if the pattern contains a syntax error.
 unsafeRegex :: String -> RegexFlags -> Regex
-unsafeRegex s f = unsafePartial fromRight (regex s f)
+unsafeRegex s f = unsafePartial fromJust (hush (regex s f))
